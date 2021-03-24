293a383c3224ed6b6a1045d649ef5cb362ec37b9
nishnat-rishi <nishnat.rishi@gmail.com>
2021-03-24 11:48:21 +0530

    Authentication setup!

--- backend_stuff/src/index.js
+++ backend_stuff/src/index.js
@@ -1,9 +1,31 @@
+require('dotenv').config()
+
 const { v1: uuid } = require('uuid')
 const {
   ApolloServer,
   UserInputError,
   gql
 } = require('apollo-server')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
+const User = require('./models/User')
+
+const jwt = require('jsonwebtoken')
+
+console.log('connecting to graphql-main...')
+
+mongoose.connect(process.env.GRAPHQL_MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(() => {
+    console.log('connected to graphql-main!')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let persons = [
   {
@@ -29,25 +51,41 @@ let persons = [
 ]
 
 const typeDefs = gql`
+
   type Person {
     name: String!
     phone: String
     address: Address!
     id: ID!
   }
+
   type Address {
     street: String!
     city: String! 
   }
+
   enum YesNo {
     YES
     NO
   }
+
+  type User {
+    username: String!
+    friends: [Person!]!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     personCount: Int!
     allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
+    me: User
   }
+
   type Mutation {
     addPerson(
       name: String!
@@ -58,25 +96,33 @@ const typeDefs = gql`
     editNumber(
       name: String!
       phone: String!
-    ): Person    
+    ): Person
+    createUser(
+      username: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }  
 `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
+    personCount: () => Person.collection.countDocuments(),
     allPersons: (root, args) => {
       if (!args.phone) {
-        return persons
+        return Person.find({})
       }
 
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone
-
-      return persons.filter(byPhone)
+      return Person.find({
+        phone: { $exists: args.phone === 'YES' }
+      })
     },
     findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+      Person.findOne({ name: args.name }),
+    me: (root, args, context) =>
+      context.currentUser
   },
   Person: {
     address: (root) => {
@@ -87,29 +133,51 @@ const resolvers = {
     }
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find(p => p.name === args.name)) {
-        throw new UserInputError('Name must be unique', {
-          invalidArgs: args.name,
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args })
+      try {
+        await person.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
         })
       }
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
       return person
     },
-    editNumber: (root, args) => {
-      const person = persons.find(p => p.name === args.name)
-      if (!person) {
-        return null
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone // this will fail on null
+      try {
+        await person.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+      return person.save()
+    },
+    createUser: (root, args) => {
+      const user = new User({ username: args.username })
+
+      return user.save()
+        .catch(error => {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+      if (!user || args.password !== 'secred') {
+        throw new UserInputError('wrong credentials')
       }
 
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map(p => p.name ===
-        args.name
-        ? updatedPerson
-        : p
-      )
-      return updatedPerson
+      const tokenData = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(tokenData, process.env.SECRET) }
     }
   }
 }
@@ -117,6 +185,20 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.SECRET
+      )
+      const currentUser = await User
+        .findById(
+          decodedToken.id
+        ).populate('friends')
+
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
---
+++ backend_stuff/src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Person'
+    }
+  ],
+})
+
+schema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', schema)
+module.exports = User
